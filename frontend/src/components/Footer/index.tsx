69f2c85d45c1c2d207be419331942d1e55e428c2
Ricardo Chies <ricardochies5@gmail.com>
2021-09-10 16:54:35 -0300

    Pagination

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/ricardo-chies" target="_blank" rel="noreferrer">Ricardo Chies</a></p>
+                <p className="text-light">Visite meu perfil no Linkedin <a href="https://www.linkedin.com/in/ricardo-chies-087557216/" target="_blank" rel="noreferrer">linkedin.com/ricardo-chies </a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
